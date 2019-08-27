f536d1a7625a0a7f529d6c1f90fe448e8830c178
prince <prince18pat@gmail.com>
2019-08-28 08:12:21 +0900

    add view controllers and layout

---
+++ FlashChat/Controller/LoginViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LoginViewController.swift
+//  FlashChat
+//
+//  Created by Prince Kumar on 2019/08/28.
+//  Copyright © 2019 Prince Kumar. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
