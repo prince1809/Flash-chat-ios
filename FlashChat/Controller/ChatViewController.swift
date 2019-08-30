9b813d720ec3a8a645be783165df3f470b591585
prince <prince18pat@gmail.com>
2019-08-31 00:46:17 +0900

    update layout

--- FlashChat/Controller/ChatViewController.swift
+++ FlashChat/Controller/ChatViewController.swift
@@ -11,22 +11,20 @@ import FirebaseAuth
 
 class ChatViewController: UIViewController {
 
+    @IBOutlet var messageTextField: UITextField!
+    @IBOutlet var sendButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    @IBAction func sendPressed(_ sender: Any) {
+        
     }
-    */
+    
     @IBAction func logOutPressed(_ sender: Any) {
         do {
             try Auth.auth().signOut()
